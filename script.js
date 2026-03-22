cd07909e8e8d447e9d5775305f42de18169e5454
Daniel <daniel81017@gmail.com>
2026-03-22 02:04:08 -0400

    alterations

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 //Mapbox Public Token
 mapboxgl.accessToken = "pk.eyJ1IjoiZGFuaWVsODEwMTciLCJhIjoiY21rZWI2eGg4MDU5NjNscHdxbjhkMTNmciJ9.jdsMukp7zHz3llySNBJs0A"
 
+//Variable declaration
+
+const center = [-79.39516, 43.66338];
+
 //Map with default center and zoom
 const map = new mapboxgl.Map(
     {
         container: 'main-map1',
         style: 'mapbox://styles/daniel81017/cmlofzis0001i01qn3jjqaxjd',
-        center: [-79.39516, 43.66338],
+        center: center,
         zoom: 14,
     }
 );
 
-//Variable for the hover event filling the campus boundary (placed here for consistency purposes)
+//Variable for the hover and click events filling the campus boundary (placed here for consistency purposes)
 let hoveredPolygonId = null;
+let clickedPolygonId = null;
 
 //Following map load, spatial data (GeoJSON file) is referenced from the remote repository, with its layers symbolized and added to the map
 map.on('load', () => {
@@ -33,28 +38,16 @@ map.on('load', () => {
     });
 
     map.addLayer({
-        'id': 'study-spots',
-        'type': 'circle',
-        'source': 'map-data',
-        'paint': {
-            'circle-width': 10,
-            'circle-color': '#0d5bd1',
-            'circle-outline': 2,
-        },
-        'filter': ['==', ['geometry-type'], 'Point'],
-    });
-
-    map.addLayer({
         'id': 'campus-fill',
         'type': 'fill',
         'source': 'map-data',
         'layout': {},
         'paint': {
-            'fill-color': '#627BC1', //CHANGE FILL COLOUR
+            'fill-color': '#c2dd13',
             'fill-opacity': [
                 'case',
                 ['boolean', ['feature-state', 'hover'], false], //INTERPRET ARRAY CORRECTLY
-                0.75,
+                0,
                 0.2
             ]
         },
@@ -72,22 +65,7 @@ map.on('load', () => {
         'filter': ['==', ['geometry-type'], 'Polygon'],
     });
 
-    map.addInteraction('study-spots-click-interaction', {
-        type: 'click',
-        target: { layerId: 'study-spots' },
-        handler: (e) => {
-            console.log("e =", e);
-            const coordinates = e.feature.geometry.coordinates.slice();
-            const description = e.feature.properties.description;
-
-            new mapboxgl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(description)
-                .addTo(map);
-        }
-    });
-
-    //Fill campus boundary with mouse hover
+    // Change the pointer to cursor when hovering over polygon features, reverting back to pointer when not hovering.
     map.on('mousemove', 'campus-fill', (e) => {
         if (e.features.length > 0) {
             if (hoveredPolygonId !== null) {
@@ -103,7 +81,6 @@ map.on('load', () => {
             );
         }
     });
-
     map.on('mouseleave', 'campus-fill', () => {
         if (hoveredPolygonId !== null) {
             map.setFeatureState(
@@ -112,14 +89,50 @@ map.on('load', () => {
             );
         }
         hoveredPolygonId = null;
-    //Zoom to feature
-    map.on('click', 'study-spots', (e) => {
-        // map.flyTo({center: [0, 0], zoom: 9});
-        map.flyTo({
-            center: e.features[0].geometry.coordinates, zoom: 20
-        });
     });
 
+    // POINT FEATURE STUFF
+    // Fly to center on campus boundary click
+    // SAMPLE FROM ABOVE, INTEGRATE W/ BELOW, USE HOVEREDCLICKID = NULL FROM ABOVE
+
+    // if (e.features.length > 0) {
+    //     if (hoveredPolygonId !== null) {
+    //         map.setFeatureState(
+    //             { source: 'map-data', id: hoveredPolygonId },
+    //             { hover: false }
+    //         );
+    //     }
+    //     hoveredPolygonId = e.features[0].id;
+    //     map.setFeatureState(
+    //         { source: 'map-data', id: hoveredPolygonId },
+    //         { hover: true }
+    //     );
+    // }
+
+    map.on('click', 'campus-fill', (e) => {
+        if (map.flyTo({
+            center: [-79.39916, 43.66226], zoom: 15, //center: e.features[0].geometry.coordinates, zoom: 20,
+        })) {}
+
+
+        console.log("alksdjflasdkjfal");
+        if (map.removeLayer('study-spots')) {
+
+        }
+
+    });
+
+    map.addLayer({
+        'id': 'study-spots',
+        'type': 'circle',
+        'source': 'map-data',
+        'paint': {
+            'circle-width': 10,
+            'circle-color': '#627BC1',
+            'circle-outline': 2,
+        },
+        'filter': ['==', ['geometry-type'], 'Point'],
+    });
     // Change the pointer to cursor when hovering over point features, reverting back to pointer when not hovering.
     map.on('mouseenter', 'study-spots', () => {
         map.getCanvas().style.cursor = 'pointer';
@@ -128,11 +141,33 @@ map.on('load', () => {
     map.on('mouseleave', 'study-spots', () => {
         map.getCanvas().style.cursor = '';
     });
-    
+
+    // Opens pop-up after clicking point
+    map.addInteraction('study-spots-click-interaction', {
+        type: 'click',
+        target: { layerId: 'study-spots' },
+        handler: (e) => {
+            const coordinates = e.feature.geometry.coordinates.slice();
+            const description = e.feature.properties.description;
+
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        }
     });
 
+    //Zoom to feature
+    map.on('click', 'study-spots', (e) => {
+        // map.flyTo({center: [0, 0], zoom: 9});
+        map.flyTo({
+            center: e.features[0].geometry.coordinates, zoom: 20
+        });
+    });
 });
 
+
+//possible else statmnt req'd for revert after exiting map also fix clicks by nesting outside of if, or remove first layer displ colour/zoom
 //Filter source: https://github.com/mapbox/mapbox-gl-js/issues/6508
 //Detached head solution: https://stackoverflow.com/questions/10228760/how-do-i-fix-a-git-detached-head
 //Mapbox GL JS Docs pop-up on click source: https://docs.mapbox.com/mapbox-gl-js/example/popup-on-click/
